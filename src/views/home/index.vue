ffe3bfd00d3e695f46e03fe74c376ec4af2a69c1
caoyang <you@example.com>
2020-01-11 17:20:29 +0800

    对创建的项目进行目录调整

---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>主页</div>
+</template>
+
+<script>
+export default {
+  name: 'home' // devtools查看组件时 可以看到的home名称
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>公共组件</div>
+</template>
+
+<script>
+export default {
+  name: 'layout'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
