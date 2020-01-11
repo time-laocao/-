b0083306f7012b3437d9eb7fbb7328d378cafc3a
caoyang <you@example.com>
2020-01-11 20:55:49 +0800

    完成Layout布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>布局</div>
+  <div class="container">
+    <!-- 可以采用Vant的导航组件 -->
+    <van-nav-bar v-if="showNavBar" fixed title="黑马头条" right-text="搜索" />
+    <!-- 二级路由容器 首页/问答/视频 /规则/我的-->
+    <div class="my-wrapper" :class="{ noTop : !showNavBar }">
+      <router-view></router-view>
+    </div>
+    <!-- 标签栏 -->
+    <van-tabbar route>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'layout'
+  name: 'layout',
+  computed: {
+    showNavBar () {
+      return this.$route.path !== '/user'
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop {
+      padding-top: 0;
+    }
+  }
+}
 </style>
