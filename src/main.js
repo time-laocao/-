93b608891390bd45d57a3731f75a100bd329fb53
caoyang <you@example.com>
2020-01-11 17:45:31 +0800

    使用Vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+// vant的样式  因为在开发的时候不知道用哪个 所以全局注册
+import 'vant/lib/index.css'
+// 全局注册在哪里都能用
+Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 
 Vue.config.productionTip = false
 
