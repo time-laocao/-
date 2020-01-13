90ddee35b8385a28513052701c9a551d9efc704c
caoyang <you@example.com>
2020-01-13 18:44:17 +0800

    登录表单效验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,25 +3,84 @@
     <van-nav-bar left-arrow title="登录" @click-left="$router.back()"></van-nav-bar>
     <!-- 手机号 验证码  登录按钮 -->
     <van-cell-group>
-      <van-field label="手机号" placeholder="请输入手机号" />
-      <van-field label="验证码" placeholder="请输入验证码">
+      <van-field
+        @blur="checkmMboile"
+        :error-message="errMsg.mobile"
+        v-model="loginForm.mobile"
+        label="手机号"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        @blur="checkCode"
+        :error-message="errMsg.code"
+        v-model="loginForm.code"
+        label="验证码"
+        placeholder="请输入验证码"
+      >
         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <div class="btn-box">
-      <van-button type="info" round  block size="small">登录</van-button>
+      <van-button type="info" @click="login" round block size="small">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 提示信息的
+      errMsg: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    checkmMboile () {
+      // 判断 为空   判断格式
+      if (!this.loginForm.mobile) {
+        this.errMsg.mobile = '手机号不能为空'
+        return false
+      } // 判断格式
+      if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errMsg.mobile = '手机格式不正确'
+        return false
+      }
+      this.errMsg.mobile = '' // 清空信息
+      return true
+    },
+    checkCode () {
+      if (!this.loginForm.code) {
+        this.errMsg.code = '验证码不能为空'
+        return false
+      }
+      if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errMsg.code = '验证码格式不正确'
+        return false
+      }
+      this.errMsg.mobile = '' // 清空信息
+      return true
+    },
+    // 登录方法
+    login () {
+      if (this.checkmMboile() && this.checkCode()) {
+        // 都通过了 表示前端校验通过了  还要调用接口
+        // 提示 登录成功
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.btn-box{
+.btn-box {
   padding: 20px;
 }
 </style>
