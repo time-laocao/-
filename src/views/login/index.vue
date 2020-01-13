30d2830a4ec5bb97c532c8dc70a4edc4dd9bccb8
caoyang <you@example.com>
2020-01-13 20:10:12 +0800

    登录逻辑

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@
     <!-- 手机号 验证码  登录按钮 -->
     <van-cell-group>
       <van-field
-        @blur="checkmMboile"
+        @blur="checkMobile"
         :error-message="errMsg.mobile"
         v-model="loginForm.mobile"
         label="手机号"
@@ -27,12 +27,14 @@
 </template>
 
 <script>
+import { login } from '@/api/user'
+import { mapMutations } from 'vuex'
 export default {
   name: 'login',
   data () {
     return {
       loginForm: {
-        mobile: '13911111111',
+        mobile: '13051281995',
         code: '246810'
       },
       // 提示信息的
@@ -43,7 +45,7 @@ export default {
     }
   },
   methods: {
-    checkmMboile () {
+    checkMobile () {
       // 判断 为空   判断格式
       if (!this.loginForm.mobile) {
         this.errMsg.mobile = '手机号不能为空'
@@ -69,12 +71,23 @@ export default {
       return true
     },
     // 登录方法
-    login () {
-      if (this.checkmMboile() && this.checkCode()) {
-        // 都通过了 表示前端校验通过了  还要调用接口
-        // 提示 登录成功
+    async login () {
+      if (this.checkMobile() && this.checkCode()) {
+        // 都通过了 表示前端校验通过 还要调用接口
+        // 提示消息 表示登录成功
+        const data = await login(this.loginForm) // 获取结果
+        // 拿到了token 更新token信息
+        // this.$store.commit('updateUser', { user: data }) // 第一种写法
+        this.updateUser({ user: data }) // 更新用户信息
+        // 登录成功
+        this.$gnotify({ type: 'success', message: '登录成功' })
+        // 跳转
+        // 两种情况 1 redirectUrl (登录未遂 => 登录  => 遂) 2 没有 redirectUrl 跳到首页
+        let { redirectUrl } = this.$route.path // 解构当前的路由信息
+        this.$router.push(redirectUrl || '/') // 短路表达式
       }
-    }
+    },
+    ...mapMutations(['updateUser'])
   }
 }
 </script>
