94585a9c2ff465271edcde00931c06a11876c2b1
caoyang <you@example.com>
2020-01-14 21:20:16 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './permission' // 经过全县处理的router
 
 import store from './store'
-import Vant from 'vant'
+import Vant, { Lazyload } from 'vant'
 // vant的样式  因为在开发的时候不知道用哪个 所以全局注册
 import 'vant/lib/index.less' // vant样式文件 换成less
 import '@/styles/index.less' // 引入全局自定义样式
@@ -14,6 +14,8 @@ import plugin from '@/utils/plugin'
 // 全局注册在哪里都能用
 Vue.use(Vant) // 全局注册  实际上调用了 Vant导出的对象的方法  install方法
 Vue.use(plugin) // 注册小插件 会调用插件中的install方法
+// 注册好了 支持v-lazy指令
+Vue.use(Lazyload)
 
 Vue.config.productionTip = false
 
