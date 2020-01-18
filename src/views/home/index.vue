e0ea0d770dce3d8bd4c97e2c45be5c72ed04e48b
caoyang <you@example.com>
2020-01-18 21:44:32 +0800

    渲染我的频道

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -21,7 +21,7 @@
     <!-- 编辑频道 -->
     <van-action-sheet :round="false" v-model="showChannelEdit" title="编辑频道">
       <!-- 放置频道编辑组件 -->
-        <channel-edit></channel-edit>
+        <channel-edit :channels="channels"></channel-edit>
     </van-action-sheet>
   </div>
 </template>
