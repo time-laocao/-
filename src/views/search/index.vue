b4b7b4c46578de4caf916091ef84bb2450e23fbf
caoyang <you@example.com>
2020-01-30 11:36:16 +0800

    搜索中心-联想搜索

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -8,8 +8,8 @@
     <van-search @click="onSearch" v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
     <!-- 联想搜索 -->
     <van-cell-group class="suggest-box"  v-if="q">
-      <van-cell icon="search">
-        <span>j</span>ava
+      <van-cell @click="tosearchResult(item)" icon="search" v-for="item in suggestList" :key="item">
+        {{item}}
       </van-cell>
     </van-cell-group>
     <!-- 历史记录 -->
@@ -35,13 +35,46 @@
 </template>
 
 <script>
+import { suggestion } from '@/api/article'
 const key = 'hm-91-toutiao-history' // 用来当做存储 本地历史记录的key
 export default {
   name: 'search',
   data () {
     return {
       q: '', /// 查询内容
-      historyList: [] // 存放历史记录的数据
+      historyList: [], // 存放历史记录的数据
+      suggestList: [] // 存放联想建议的数组
+    }
+  },
+  watch: {
+    // 防抖搜索
+    // q () {
+    //   // 防抖代码以下
+    //   clearTimeout(this.timer)
+    //   this.timer = setTimeout(async () => {
+    //     // 搜索联想的词汇
+    //     if (this.q) {
+    //       this.suggestList = [] // 当前搜索关键字变成空 的时候 将联想数字清空
+    //     }
+    //     let data = await suggestion({ q: this.q })
+    //     this.suggestList = data.options
+    //   }, 500)
+    // }
+
+    // 节流
+    q () {
+      if (!this.timer) {
+        this.timer = setTimeout(async () => {
+          this.timer = null
+          // 联想搜索的词汇
+          if (!this.q) {
+            this.suggestList = []
+            return false // 直接返回下面不在执行
+          }
+          let data = await suggestion({ q: this.q })
+          this.suggestList = data.options
+        }, 500)
+      }
     }
   },
   methods: {
@@ -70,6 +103,15 @@ export default {
       localStorage.setItem(key, JSON.stringify(this.historyList)) // 重新写入缓存
       //  也应该去搜索结果页面 而且 也要携带参数
       this.$router.push({ path: '/search/result', query: { q: this.q } }) // 直接跳转到搜索结果界面
+    },
+    // 点击联想搜索关键字 去跳转 =》 先把点击的关键词 放入历史记录 表示我搜索过
+    tosearchResult (text) {
+      // 放入历史记录
+      let obj = new Set(this.historyList) // 生成一个set变量  se t对象自动去重
+      obj.add(text) // 加入历史记录
+      this.historyList = Array.from(obj) // 将set转回数组
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 重新写入缓存
+      this.$router.push({ path: '/search/result', query: { q: text } }) // 直接跳转到搜索结果界面
     }
   },
   created () {
